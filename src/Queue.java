e2083590b80ec4b6ccf44e08675dbc792337b1df
sofia-covarrubias <sofiacovarrubias1987@gmail.com>
2022-02-15 08:33:46 -0800

    Initial commit

---
+++ src/Queue.java
@@ -0,0 +1,32 @@
+public class Queue
+{
+    private class Node
+    {
+        // instance variables
+        private int data;
+        private Node link; // self-reference
+
+        // constructor
+        public Node (int data, Node link)
+        {
+            this.data = data;
+            this.link = link;
+        }
+
+        // getter methods
+        public int getData ()
+        {
+            return data;
+        }
+        public Node getLink ()
+        {
+            return link;
+        }
+
+        // to string method
+        public String toString ()
+        {
+            return "Data: " + data + " is linked to " + link;
+        }
+    }
+} // end main Queue class
---
+++ src/QueueDriver.java
@@ -0,0 +1,8 @@
+public class QueueDriver
+{
+    public static void main (String[] args)
+    {
+
+    } // end main method
+
+} // end main class
