c2d6cbb7ec844e16f4deb031921d39409ec52e89
sofia-covarrubias <sofiacovarrubias1987@gmail.com>
2022-02-15 08:41:09 -0800

    basics of queue public class : constructor/variables/main actions

--- src/Queue.java
+++ src/Queue.java
@@ -1,5 +1,39 @@
 public class Queue
 {
+    // variables
+    private Node head;
+    private Node tail;
+
+    // constructor
+    public Queue ()
+    {
+        head = null;
+        tail = null;
+    } // end constructor
+
+    // populate list and insertion of nodes
+    public void addToTail (int newNode)
+    {
+        tail = new Node(newNode, tail);
+    } // end addNodeToTail method
+
+    public void deleteFromHead ()
+    {
+        if (head != null)
+        {
+            head.getLink();
+        }
+        else
+        {
+            System.out.println("Do you want to delete this from an empty list? ");
+            System.exit(0); // We can pass any integer as an argument to the method
+        }
+    }
+
+
+
+    // ---------------------------------------------------------
+    // PRIVATE CLASS
     private class Node
     {
         // instance variables
