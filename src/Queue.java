d2621ad242005d6a7cf7a3edbce9c736be517d30
sofia-covarrubias <sofiacovarrubias1987@gmail.com>
2022-02-25 08:54:49 -0800

    redid structure

--- src/Queue.java
+++ src/Queue.java
@@ -1,23 +1,155 @@
 public class Queue
 {
+    private Node head;
+    private Node tail;
+    private static int size;
+
+    public Queue ()
+    {
+        head = null;
+        tail = null;
+        size = 0;
+    } // end Queue constructor
+
+    public void enqueue (int newData)
+    {
+        tail = new Node(newData, tail);
+        if (size == 0)
+        {
+            head = tail;
+        } // end if
+        size++;
+    } // end enqueue
+
+    public void dequeue ()
+    {
+        Node temp;
+        if (size != 0)
+        {
+            temp = head;
+        }
+        head = head.getLink();
+    } // end dequeue
+
+    public static int getSize ()
+    {
+        return size;
+    } // end getSize
+
+    public void peekHead ()
+    {
+        if (head != null)
+        {
+            System.out.println(head.getData());
+        }
+    } // end peekHead
+
+    public void peekTail ()
+    {
+        if (tail != null)
+        {
+            System.out.println(tail.getData());
+        }
+    } // end peekTail
+
+    public boolean hasNext ()
+    {
+        Node pos = head;
+        if (pos.getLink() == null)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    } // end hasNext
+
+    public void show ()
+    {
+        Node position  = head;
+        while (position != null)
+        {
+            System.out.println(position.getData());
+            position = position.getLink();
+        }
+    }
+
+
+
+    private class Node
+    {
+        private int data;
+        private Node link;
+
+        public Node (int data, Node link)
+        {
+            this.data = data;
+            this.link = link;
+        } // end Node constuctor
+
+        public int getData ()
+        {
+            return data;
+        } // end getter
+        public Node getLink ()
+        {
+            return link;
+        } // end getter
+
+        public String toString ()
+        {
+            return "Data: " + data + " is linked to " + link;
+        } // end toString
+    } // end Node calss
+} // end Queue class
+/*
+public class Queue
+{
     // variables
     private Node head;
     private Node tail;
+    private static int size;
 
     // constructor
     public Queue ()
     {
         head = null;
         tail = null;
+        this.size = 0;
     } // end constructor
 
+    public void enqueue (int newNode) {
+        if (size == 0)
+        {
+
+        }
+        tail = new Node (newNode, tail);
+    }
+
+    public void dequeue ()
+    {
+        if (head != null)
+        {
+            head.getLink();
+        }
+        else
+        {
+            System.out.println("There is nothing to delete. This is an empty list.");
+            System.exit(0);
+        }
+    }
+
+
+    // MAIN BRAIN METHODS
+    /*
     // populate list and insertion of nodes
-    public void addToTail (int newNode)
+    public void addToTail (int newNode) // you add to the tail / rear (first in, first out)
     {
         tail = new Node(newNode, tail);
     } // end addNodeToTail method
 
-    public void deleteFromHead ()
+    public void deleteFromHead () // you delete from the head / front (first in, first out)
     {
         if (head != null)
         {
@@ -28,7 +160,21 @@ public class Queue
             System.out.println("Do you want to delete this from an empty list? ");
             System.exit(0); // We can pass any integer as an argument to the method
         }
-    }
+    } // end deleteFromHead method
+
+    public boolean hasNext ()
+    {
+        Node index = head;
+        if (index.getLink() != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    } // end hasNext method
+
 
 
 
@@ -46,6 +192,8 @@ public class Queue
             this.data = data;
             this.link = link;
         }
+
+
 
         // getter methods
         public int getData ()
@@ -65,3 +213,4 @@ public class Queue
     } // end Node class
 
 } // end main Queue class
+*/
--- src/QueueDriver.java
+++ src/QueueDriver.java
@@ -3,6 +3,7 @@ public class QueueDriver
     public static void main (String[] args)
     {
 
+
     } // end main method
 
 } // end main class
